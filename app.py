52bff1813046382847cb74e3c7c960ba0a137fca
Bruno V <brunovieira1@usp.br>
2024-06-18 22:06:00 -0300

    Adding features

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-
+import time
 import streamlit as st
 from extract_text import transcribe_audio
 from process_text import process_text
@@ -6,21 +6,46 @@ from process_text import process_text
 # Title
 st.title("Transcritor de Áudio")
 
+# Customer guidelines
+st.write("Grave sua consulta utilizando o gravador de áudio do celular ou de seu computador, e em seguida, abra-o abaixo.")
+
+
 # File Upload
 uploaded_file = st.file_uploader("Escolha um arquivo de áudio...", type=["wav", "mp3", "m4a"])
 
 if uploaded_file is not None:
-    # Step 1: Extract text from the uploaded audio file
-    st.write("Transcrevendo áudio...")
-    transcribed_text = transcribe_audio(uploaded_file)
-    st.write("Transcrição Completa!\nIniciando Processamento...")
     
-    # Step 2: Process the extracted text using ChatGPT
+    # Step 1: Start the transcription process
+    with st.spinner("Transcrevendo áudio..."):
+        st.warning("A transcrição pode demorar um pouco.")
+        start_time = time.time()
+        transcribed_text = transcribe_audio(uploaded_file)
+        end_time = time.time()
+        actual_time = end_time - start_time
+
+    # Step 2: Display timeout and completion message
+    
+    st.write("Transcrição Completa!\nIniciando Processamento...")
+
+    st.write(f"Tempo real de transcrição: {actual_time:.2f} segundos.")
+    
+    
+    # Step 3: Process the extracted text using ChatGPT
     st.write("Processando o texto...")
-    processed_text = process_text(transcribed_text)
-    st.write("Análise Pronta: ")
+    processed_text,improved_transcript = process_text(transcribed_text)
+    st.write("*Análise Pronta:* ")
     st.write(processed_text)
 
-    # Step 3: Pasting the transcript
-    st.write("\n\n\n\nTranscrição:")
-    st.write(transcribed_text)
+    # Step 4: Pasting the transcript
+    st.write("\n\n\n\nTranscrição Processada:")
+    st.write(improved_transcript)
+
+
+    # Step 5: Feedback
+    st.write("Como podemos melhorar?\n\n\n")
+
+    feedback = st.chat_input("Deixe aqui seu feedback.")
+
+    feedback1 = st.text_input("Deixe aqui seu feedback.")
+
+    
--- process_text.py
+++ process_text.py
@@ -11,24 +11,53 @@ client = OpenAI(api_key=os.environ["OPENAI_API_KEY"])
 
 def process_text(text):
 
+    #1 Improve Transcription
+
+    response = client.chat.completions.create(
+        model="gpt-4o",
+        messages=[
+            {"role": "system",
+              "content": ("""Essa é uma transcrição de um áudio. Com o mínimo de 
+                          mudanças necessário, verifique se há constância das regras
+                           gramaticais, ajustando o texto conforme necessário. Quero
+                           também que verifique o contexto, e caso encontre palavras 
+                          que soem similares mas que uma delas não faz sentido, mude-a
+                           para manter o contexto. Lembre-se que há um médico 
+                          perguntando e um paciente (ou mais) respondendo. Mas repito,
+                           só faça isso quando tiver muita segurança, como no exemplo 
+                          a seguir: 'Para dor de cabeça, recomendo para se ta mole.'->
+                           'Para dor de cabeça, recomendo paracetamol.' Caso contrário,
+                          não mude nada. Essa é uma tarefa delicada, então vá devagar
+                          e passo a passo."""
+                )
+            },
+            {"role": "user", "content": f"{text}"}],
+        stream=False,
+    )
+    improved_transcription = response.choices[0].message.content
+
+    #2 Process the transcription
+
     response = client.chat.completions.create(
         model="gpt-4o",
         messages=[
             {"role": "system",
               "content": (
                   """Você é um ajudante para médicos, e vai organizar os dados de
-                    uma consulta.\n Extraia todas as informações básicas do paciente,
+                    uma consulta. Extraia todas as informações básicas do paciente,
                       e crie um segmento (estilo markdown) para mostrar estas 
-                      informações, como a seguir:\n(entenda cada \n como um line break)
+                      informações, como a seguir:(entenda cada como um line break)
                       Nome: Alberto Marinho\n Idade: 39 anos\n Gênero: Masculino\n
                       Contato: ...\n\n Em seguida, crie outro segmento para 
                       a história médica, e depois um para Sintomas e Razão Para Visita.
-                      Caso não encontres alguma informação, coloque o campo vazio,
-                      desse jeito: Gênero:           \n"""
+                      Caso não encontre alguma informação, coloque o campo vazio. 
+                      Dê mais ênfase para as informações que o médico fala quando
+                      encontrar dados conflitantes."""
                 )
             },
-            {"role": "user", "content": f"{text}"}],
+            {"role": "user", "content": f"{improved_transcription}",}],
         stream=False,
     )
     content = response.choices[0].message.content
-    return content
+
+    return content, improved_transcription
